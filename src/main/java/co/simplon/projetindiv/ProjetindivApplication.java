d1f1af006e0fc77c52cf375471596eb0fe00fd50
LOUISL <laetitia.louis.lp4@gmail.com>
2019-12-11 17:40:10 +0100

    commit initial

---
+++ src/main/java/co/simplon/projetindiv/ProjetindivApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.projetindiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetindivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetindivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/simplon/projetindiv/ProjetindivApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.projetindiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetindivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
